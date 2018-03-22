b965840617e5de332f2d58f9bada30570d3b8c6d
Martyna Jasiak <martyna.jasiak@gmail.com>
2018-03-22 15:07:57 +0100

    First eX

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-/*var player_one = true;
+var player_X = 1;
+//var square = document.getElementById('0');
 
-function display() {
+
+function display(cell) {
+    var square = document.getElementsByClassName("cell")[0];
+   
+    if(square.innerHTML !="") return;
     
-}*/
+    if ( player_X == 1){
+        square.innerHTML = "X";
+        player_X = 0;
+    } else {
+        square.innerHTML = "O";
+        player_X = 1;
+    }
+}
+
