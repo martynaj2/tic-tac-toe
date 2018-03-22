20b6f9a364844af9e125aac6f1c2227db41f3097
Martyna Jasiak <martyna.jasiak@gmail.com>
2018-03-22 15:38:09 +0100

    getting there

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 var player_X = 1;
 //var square = document.getElementById('0');
 
@@ -15,4 +16,25 @@ function display(cell) {
         player_X = 1;
     }
 }
+*/
 
+
+var something = document.getElementsByClassName('cell')[3];
+/*for (var i=0; i<something.length; i++){
+    
+}*/
+/*var something = document.querySelectorAll(".cell");*/
+var player_x = 1;
+something.addEventListener('click', put_xy);
+
+function put_xy() {
+    if (something.innerHTML !== "") return;
+    if (player_x == 1) {
+        something.innerHTML = "X";
+        player_x = 0;
+    }
+    else {
+        something.innerHTML = "0";
+        player_x = 1;
+    }
+}
