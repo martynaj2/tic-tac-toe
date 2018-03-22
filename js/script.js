b2b8d6ea4efded031de569ad03daea7276db7c64
Martyna Jasiak <martyna.jasiak@gmail.com>
2018-03-22 22:26:52 +0100

    stage 1 - finished

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var something = document.getElementsByClassName('cell');
+
 for (var i=0; i<something.length; i++){
     something[i].addEventListener('click', put_xy);
 }
@@ -6,23 +7,30 @@ for (var i=0; i<something.length; i++){
 var player_x = 1;
 
 function put_xy(event) {
-   //console.log(23232);
-    //ert(event.target.id);
     var square = document.getElementById(event.target.id);
-    if (player_x == 1) {
-        square.classList.remove('playero');
-        square.classList.add('playerx');
-        square.innerHTML = "X";
-        player_x = 0;
+    if (square.innerHTML != "") return;
+        if (player_x == 1) {
+            square.classList.remove('playero');
+            square.classList.add('playerx');
+            square.innerHTML = "X";
+            player_x = 0;
+        }
+        else {
+            square.classList.remove('playerx');
+            square.classList.add('playero');
+            square.innerHTML = "O";
+            player_x = 1;
+        }   
     }
-    else {
-        square.classList.remove('playerx');
-        square.classList.add('playero');
-        square.innerHTML = "O";
-        player_x = 1;
+
+var reset = document.getElementById("resetBtn");
+reset.addEventListener('click',resetgame);
+
+
+
+function resetgame() {
+    for (var i=0; i<something.length; i++){
+    something[i].innerHTML="";
     }
-     
 }
-
-
 
